08b4dcaf1bbb484d68f0a7b676473d04e864dcb3
moccocino <81292551+moccocino@users.noreply.github.com>
2021-05-27 11:23:01 +0300

    Delete MyArrayDataException.java

--- src/main/java/Lesson2/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package Lesson2;
-
-public class MyArrayDataException extends Exception{
-    public MyArrayDataException(int i, int j) {
-        System.out.println("В ячейке " + i + ", " +j+ " содержится текстовая информация.");
-    }
-}
